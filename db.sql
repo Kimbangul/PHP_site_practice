e150ce5dfbe9e307a8b5a93bdb63a03f66bc2fce
Kimbangul <highcolor_12@naver.com>
2020-08-30 15:58:22 +0900

    게시물 리스팅

--- db.sql
+++ db.sql
@@ -46,20 +46,21 @@ CREATE TABLE `article`(
     id INT(10) UNSIGNED NOT NULL PRIMARY KEY AUTO_INCREMENT, # 번호
     regDate DATETIME NOT NULL, # 작성날짜
     updateDate DATETIME NOT NULL, # 수정날짜
+    parentId INT(10) UNSIGNED NOT NULL,
     boardId INT(10) UNSIGNED NOT NULL, #게시판 아이디
-      memberId INT(10) UNSIGNED NOT NULL, # 작성자 ID
-       `body` TEXT NOT NULL, #내용  
-       `replyBody` TEXT NOT NULL, #응답내용
+    memberId INT(10) UNSIGNED NOT NULL, # 작성자 ID
+    `body` TEXT NOT NULL, #내용  
 
+    `title` CHAR(100) NOT NULL, #제목
     displayStatus TINYINT(1) UNSIGNED NOT NULL, # 노출상태
     delStatus TINYINT(1) UNSIGNED NOT NULL, # 삭제상태
     delDate DATETIME NOT NULL, # 삭제날짜
-    typeCode CHAR(20) NOT NULL, # 1차 카테고리
-    type2Code CHAR(20) NOT NULL, # 2차 카테고리
-    readStatus tinyint(1) unsigned not null, # 수신자의 읽기 상태
-    readDate datetime not null, # 수신자의 읽은 날짜
-    completeStatus tinyint(1) unsigned not null, # 완료 상태
-    completeDate datetime not null, # 완료 날짜
+    cateCode CHAR(20) NOT NULL, # 1차 카테고리
+    cate2Code CHAR(20) NOT NULL, # 2차 카테고리
+    readStatus TINYINT(1) UNSIGNED NOT NULL, # 수신자의 읽기 상태
+    readDate DATETIME NOT NULL, # 수신자의 읽은 날짜
+    completeStatus TINYINT(1) UNSIGNED NOT NULL, # 완료 상태 
+    completeDate DATETIME NOT NULL, # 완료 날짜
     writerName CHAR(20) NOT NULL, # 작성자 이름
      writerEmail CHAR(100) NOT NULL, # 작성자 이메일
       writerPhone CHAR(20) NOT NULL, # 작성자 전화번호
@@ -67,3 +68,13 @@ CREATE TABLE `article`(
          writerSnsType CHAR(20) NOT NULL # 작성자 sns 타입
 );
 
+
+
+INSERT INTO `article`
+SET regDate = NOW(),
+    updateDate = NOW(),
+    boardId = 1,
+    memberId = 1,
+    displayStatus = 1,
+    title = '제목1',
+    `body` = '내용1'; 
--- lib/lib.php
+++ lib/lib.php
@@ -1,67 +1,137 @@
 <?php
-
-function jsAlert(string $msg){
+function jsAlert(string $msg) {
     echo "<script> alert('{$msg}'); </script>";
 }
-
-function jsLocationReplace(string $url){
+function jsLocationReplace(string $url) {
     echo "<script> location.replace('{$url}'); </script>";
     exit;
 }
-
-function jsHistoryBack(){
-    echo "<script>history.back();</script>";
+function jsHistoryBack() {
+    echo "<script> history.back(); </script>";
     exit;
 }
-
-function DB__execute($sql){
+function DB__execute($sql) {
     global $config;
-    // 외부에서 선언된 $config 변수를 사용하겠다.
     return mysqli_query($config['dbConn'], $sql);
 }
-
 function DB__insert($sql) {
     global $config;
     DB__execute($sql);
     return mysqli_insert_id($config['dbConn']);
 }
-
 function DB__update($sql) {
     DB__execute($sql);
 }
-
 function DB__delete($sql) {
     DB__execute($sql);
 }
-
-
-function DB__getDBRows($sql){
-
+function DB__getDBRows($sql) {
     $rs = DB__execute($sql);
     $rows = [];
-
-    while ($row = mysqli_fetch_assoc($rs)){
+    while ( $row = mysqli_fetch_assoc($rs) ) {
         $rows[] = $row;
     }
-
     return $rows;
 }
-
-function DB__getDBRow($sql){
+function DB__getDBRow($sql) {
     $rows = DB__getDBRows($sql);
-
-    if (isset($rows[0])){
+    if ( isset($rows[0]) ) {
         return $rows[0];
     }
     return [];
 }
 
-function filterSqlInjection(&$args){
-    global $config;
-//  & -> 복사된 값이 아닌 원본 배열의 값을 넘겨받아 수정하겠다.
+function DB__getDBRowIntValue($sql, $default) : int {
+    $row = DB__getDBRow($sql);
 
-   foreach ($args as $key => $val){
-       $args[$key] = mysqli_real_escape_string($config['dbConn'], $val);
-   }
+    if ( empty($row) ) {
+        return $default;
+    }
 
+    foreach ( $row as $val ) {
+        return $val;
+    }
 }
+
+function DB__getDBRowStringValue($sql, $default) : string {
+    $row = DB__getDBRow($sql);
+
+    if ( empty($row) ) {
+        return $default;
+    }
+
+    foreach ( $row as $val ) {
+        return $val;
+    }
+}
+
+function filterSqlInjection(&$args) {
+    global $config;
+    foreach ( $args as $key => $val ) {
+        $args[$key] = mysqli_real_escape_string($config['dbConn'], $val);
+    }
+}
+
+function getArrValue(&$arr, $key, $default) {
+    if ( isset($arr[$key]) ) {
+        return $arr[$key];
+    }
+
+    return $default;
+}
+
+function getQueryStringFromParam($params) : string {
+    $queryStr = '';
+
+    foreach ( $params as $key => $val ) {
+        if ( $queryStr ) {
+            $queryStr .= '&';
+        }
+        $queryStr .= $key . '=' . $val;
+    }
+
+    return $queryStr;
+}
+
+function getNewUri(string $url, string $paramKey, string $paramValue) {
+    $urlInfo = getUrlInfo($url);
+
+    $urlInfo['queryParams'][$paramKey] = $paramValue;
+
+    return $urlInfo['url'] . '?' . getQueryStringFromParam($urlInfo['queryParams']);
+}
+
+function getUrlInfo(string $url) {
+    if ( strpos($url, '?') === false ) {
+        $url .= "?";
+    }
+
+    list($url, $queryStr) = explode('?', $url);
+
+    $queryStrBtis = explode('&', $queryStr);
+    $queryParams = [];
+
+    foreach ( $queryStrBtis as $paramStr ) {
+        $paramStrBits = explode('=', $paramStr);
+
+        $key = $paramStrBits[0];
+        if ( $key ) {
+            $value = '';
+            if ( isset($paramStrBits[1]) ) {
+                $value = $paramStrBits[1];
+            }
+            $queryParams[$key] = $value;
+        }
+    }
+
+    $info = [];
+    $info['url'] = $url;
+    $info['queryStr'] = $queryStr;
+    $info['queryParams'] = $queryParams;
+
+    return $info;
+}
+
+function isE(&$arr, $key) {
+    return isset($arr[$key]) and !empty($arr[$key]) ;
+} 
